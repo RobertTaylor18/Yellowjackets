8e367fdab314dd7f8e20b0d98454f22a0352602e
RobertTaylor <56117726+RobertTaylor18@users.noreply.github.com>
2021-04-06 16:34:03 +0100

    Added early draft for hp bars and damage numbers

--- Yellowjackets Game/Assets/Scripts/DamageCalc/Health.cs
+++ Yellowjackets Game/Assets/Scripts/DamageCalc/Health.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
 
-    public int health;
-    public int maxhealth;
+    public float health;
+    public float maxhealth;
 
     public Inventory inventory;
     public float moneyValue;
@@ -14,12 +15,20 @@ public class Health : MonoBehaviour
     public GameObject FloatingTextPrefab;
     public GameObject player;
 
+    public GameObject healthBarCanvas;
+    public GameObject healthBarUI;
+    public GameObject healthBarObj;
+    public float uiAlpha = 0;
+    public bool isFading = false;
+    public Image healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
         health = maxhealth;
         player = GameObject.FindWithTag("Player");
         inventory = player.GetComponent<Inventory>();
+        spawnHealthBar();
     }
 
     void FixedUpdate()
@@ -27,14 +36,61 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             inventory.money += moneyValue;
-            Destroy(this.gameObject);
+            Destroy(this.gameObject);   
         }
+        healthBarUI.transform.LookAt(player.transform);
+        healthBarUI.transform.rotation = Quaternion.LookRotation(player.transform.forward);
+        
+        
+    }
+
+    void Update()
+    {
+        
+            
+        
+        healthBarUI.GetComponent<CanvasGroup>().alpha = uiAlpha;
+        healthBar.fillAmount = health / maxhealth;
+
+        if (isFading && uiAlpha >= 0)
+        {
+                uiAlpha -= Time.deltaTime;
+        }
+        else
+        {
+            isFading = false;
+        }
+        
     }
 
     public void OnDamage()
     {
+        uiAlpha = 1;
+        StartCoroutine(timer());
         var go = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
         go.GetComponent<TextMesh>().text = health.ToString();
         go.transform.LookAt(player.transform);
+        go.transform.rotation = Quaternion.LookRotation(player.transform.forward);
+
+        
+    }
+
+    public void spawnHealthBar()
+    {
+        var go = Instantiate(healthBarCanvas, transform.position, Quaternion.identity, transform);
+        healthBarUI = go.GetComponentInChildren<CanvasGroup>().gameObject;
+        // healthBarObj = healthBarUI.GetComponentInChildren<Image>().gameObject;
+
+
+        healthBarObj = healthBarUI.transform.GetChild(1).gameObject;
+        healthBar = healthBarObj.GetComponent<Image>();
+        
+    }
+
+    IEnumerator timer()
+    {
+        yield return new WaitForSeconds(3f);
+
+        isFading = true;
     }
 }
