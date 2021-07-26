8d88c19be70d20dbf441043387ff64189d35ef43
JoshIsAStudent <43608508+DerpyJish@users.noreply.github.com>
2021-07-26 23:03:19 +0100

    Update LookBehind.cs

--- Yellowjackets Game/Assets/Scripts/LookBehind.cs
+++ Yellowjackets Game/Assets/Scripts/LookBehind.cs
@@ -2,38 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookBehind : MonoBehaviour
-{
+public class LookBehind : MonoBehaviour {
+    public GameObject mainCamera;
+    public GameObject rearCamera;
 
-    public GameObject camera;
-    public GameObject camera2;
-    //public GameObject camera3;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey("f"))
-        {
-            camera.SetActive(false);
-            camera2.SetActive(true);
-            //camera3.SetActive(false);
-        }
-        else if (Input.GetKey("b"))
-        {
-            camera.SetActive(false);
-            camera2.SetActive(false);
-            //camera3.SetActive(true);
-        }
-        else
-        {
-            camera.SetActive(true);
-            camera2.SetActive(false);
-            //camera3.SetActive(false);
-        }
+    void Update() {
+        bool rearView = Input.GetKey("f");
+        mainCamera.SetActive(!rearView);
+        rearCamera.SetActive(rearView);
     }
 }
